1a9a63dc330d9dc045dc22e7548da6d81222e8ae
Max G <gmax0687@gmail.com>
2020-06-23 20:08:24 -0500

    added pretty much all missing functionality, such as cat's games, and determining who won

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const gameBoard = (() => {
-    let gameArray = [
-        [" ", " ", " "],
-        [" ", " ", " "],
-        [" ", " ", " "],
+    let originalArray = [
+        ["  ", "  ", " "],
+        [" ", " ", "  "],
+        ["  ", " ", " "],
     ];
-
-    return { gameArray };
+    let gameArray = [
+        ["  ", "  ", " "],
+        [" ", " ", "  "],
+        ["  ", " ", " "],
+    ];
+    return { gameArray, originalArray };
 })();
 
 const displayController = (() => {
@@ -22,44 +26,112 @@ const displayController = (() => {
                 const symbol = document.createElement("p");
                 symbol.classList.add(j);
                 symbol.classList.add(i);
-                symbol.id = k;
+                symbol.id = "num" + k;
                 gameSpace.appendChild(symbol);
-                document.getElementById(k).textContent = board[i][j];
+                document.getElementById("num" + k).textContent = board[i][j];
                 k++;
             }
         }
     }
     render();
-    return { render };
+
+    document.addEventListener("click", choosePlacement);
+    function choosePlacement(evt) {
+        let xVal = evt.srcElement.classList[0];
+        let yVal = evt.srcElement.classList[1];
+        if (yVal === undefined) {
+            yVal = xVal;
+        }
+        logic.testPlacement(xVal, yVal, evt);
+    }
+
+    function resetBoard() {
+        gameBoard.gameArray = gameBoard.originalArray;
+        console.log(gameBoard.gameArray);
+        console.log(gameBoard.originalArray);
+    }
+
+    function endGame(state) {
+        document.removeEventListener("click", choosePlacement);
+        const messageArea = document.querySelector("#messageArea");
+
+        if (state === "win") {
+            messageArea.textContent = "Congrats, you won!";
+        } else {
+            messageArea.textContent = "Cat's Game";
+        }
+    }
+    return { render, resetBoard, endGame };
 })();
 
 const player = (name, symbol) => {
     return { name, symbol };
 };
 
-//const logic = (() => {
-(() => {
-    const player1 = player("joe", "x");
-    //const player2 = player(prompt("Choose name"), "o");
-    let symbol = player1.symbol;
+const logic = (() => {
     let board = gameBoard.gameArray;
 
-    document.addEventListener("click", playerChoice);
+    const player1 = player("joe", "x");
+    const player2 = player("mama", "o");
+    let symbol = player1.symbol;
 
-    function playerChoice(evt) {
-        let xVal = evt.srcElement.classList[0];
-        let yVal = evt.srcElement.classList[1];
-        if (yVal === undefined) {
-            yVal = xVal;
+    let turnCounter = 0;
+    function getTurn() {
+        if (turnCounter % 2 === 0) {
+            symbol = player2.symbol;
+        } else {
+            symbol = player1.symbol;
         }
+        turnCounter++;
+        if (turnCounter > 4) {
+            getCurrentWinBoards();
+        }
+    }
 
+    function testPlacement(xVal, yVal, evt) {
         if (evt.srcElement.localName === "p") {
-            if (board[yVal][xVal] === " ") {
+            if (board[yVal][xVal] === " " || board[yVal][xVal] === "  ") {
                 board[yVal].splice(xVal, 1, symbol);
+                getTurn();
+                displayController.render();
+                console.log(true);
             }
         }
-
-        displayController.render();
     }
-    return {};
+
+    function getCurrentWinBoards() {
+        let possibleBoards = [
+            board[0],
+            board[1],
+            board[2],
+            [board[0][0], board[1][0], board[2][0]],
+            [board[0][1], board[1][1], board[2][1]],
+            [board[0][2], board[1][2], board[2][2]],
+            [board[0][0], board[1][1], board[2][2]],
+            [board[0][2], board[1][1], board[2][0]],
+        ];
+        testForWinner(possibleBoards);
+    }
+
+    function testForWinner(winBoards) {
+        const allEqual = (arr) => arr.every((v) => v === arr[0]);
+        for (const element of winBoards) {
+            if (allEqual(element)) {
+                displayController.endGame("win");
+                break;
+            } else if (turnCounter === 9) {
+                displayController.endGame("tie");
+            }
+        }
+    }
+
+    return { testPlacement, getCurrentWinBoards };
+})();
+
+const AI = (() => {
+    const simClick = document.getElementById("simClick");
+    simClick.addEventListener("click", simAI);
+    function simAI() {
+        document.getElementById("num" + Math.floor(Math.random() * 9)).click();
+    }
 })();
